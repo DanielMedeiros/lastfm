6689b4cac43bf8a0ca131b83c3d9c80ab4926966
Daniel Medeiros <daniel@human2be.com.br>
2018-11-26 17:40:55 -0200

    lastfm

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,37 +2,35 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 
 
 class APIController extends Controller
 {
-    const LASTFM_API_BASEURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=aefa54108190e9c1d8dd093e5f628a21&limit=30&format=json&callback=?';
+    const LASTFM_API_BASEURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=aefa54108190e9c1d8dd093e5f628a21&limit=10&format=json&callback=';
 
-	protected $client;
+    protected $client;
 
-	public function __construct()
-	{
-	   	$this->client = new Client(); 
-	}
+    public function __construct()
+    {
+        $this->client = new Client(); 
+    }
 
     public function getClientesAPI()
     {
-    	$result = $this->client->get(self::LASTFM_API_BASEURL);
+        $result = $this->client->get(self::LASTFM_API_BASEURL);      
 
-       /* if (200 !== $result->getStatusCode() || null === $result->data) {
-            $this->throwResponseException();
-        }*/
-    		
-        echo $result->getBody()->getContents();
-        
-      /*  $resultado = json_decode((string) $result->getBody()->getContents(), true);
+       $resultados = json_decode((string)$result->getBody(), true);
+       /*dd($resultados);*/
 
-        echo $resultado['artist'];*/
-/*
-        foreach($resultado as $result){
-            echo "<p>" . $result->artist . " - <b>".  $result->album ."</b></p><br>";
-        }*/
+        foreach($resultados['topartists']['artist'] as $resultado){
+            echo "<p>" . $resultado['name'] . " - <b>".  $resultado['url'] ."</b></p><br>";     
+        }       
+    
+
   
     }
 
